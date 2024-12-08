9986d4266b557c03863218da96119bba5764a321
oleksii <61844150+oleksii02@users.noreply.github.com>
2024-12-09 00:19:11 +0200

    Update firebase.ts

--- src/shared/lib/firebase/firebase.ts
+++ src/shared/lib/firebase/firebase.ts
@@ -3,14 +3,15 @@ import { initializeApp } from 'firebase/app';
 
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyCzwN5SlqyFp70tdsOBf07rDCmjfyZ7uJo',
+  authDomain: 'todo-list-a4748.firebaseapp.com',
+  databaseURL:
+    'https://todo-list-a4748-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'todo-list-a4748',
+  storageBucket: 'todo-list-a4748.firebasestorage.app',
+  messagingSenderId: '883898799460',
+  appId: '1:883898799460:web:e45913e1653acd69139671',
+  measurementId: 'G-L8480HHBQ5',
 };
 
 export const app = initializeApp(firebaseConfig);
